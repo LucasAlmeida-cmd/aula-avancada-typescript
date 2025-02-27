be81ad80710f8b0d8cc12b02335e309d291d7a77
Lucas-CMD <lucasalmeidafmoraes@gmail.com>
2025-02-27 20:41:16 -0300

    Cria classe GerenciadorVeiculos para gerenciar lista de veículos

---
+++ src/classes/GerenciadorVeiculos.ts
@@ -0,0 +1,20 @@
+// src/classes/GerenciadorVeiculos.ts
+export class GerenciadorVeiculos<T> {
+    private veiculos: T[] = [];
+  
+    adicionar(veiculo: T): void {
+      this.veiculos.push(veiculo);
+    }
+  
+    remover(veiculo: T): void {
+      const index = this.veiculos.indexOf(veiculo);
+      if (index > -1) {
+        this.veiculos.splice(index, 1);
+      }
+    }
+  
+    listarVeiculos(): void {
+      console.log(this.veiculos);
+    }
+  }
+  
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,36 @@
 import { Carro } from "./interface/Carro";
 import { Moto } from "./interface/Moto";
-
+import { GerenciadorVeiculos } from "./classes/GerenciadorVeiculos";
 
 const meuCarro: Carro = {
-    marca: "Toyota",
-    modelo: "Corolla",
-    ano: 2022,
-    portas: 4,
-    acelerar: function () {
-      return `${this.marca} ${this.modelo} está acelerando!`;
-    }
-  };
+  marca: "Toyota",
+  modelo: "Corolla",
+  ano: 2022,
+  portas: 4,
+  acelerar: function () {
+    return `${this.marca} ${this.modelo} está acelerando!`;
+  },
+};
 
-  const minhaMoto: Moto = {
-    marca: "Honda",
-    modelo: "CB 500F",
-    ano: 2023,
-    cilindradas: 500,
-    acelerar: function () {
-      return `${this.marca} ${this.modelo} está acelerando!`;
-    }
-  };
+const minhaMoto: Moto = {
+  marca: "Honda",
+  modelo: "CB 500F",
+  ano: 2023,
+  cilindradas: 500,
+  acelerar: function () {
+    return `${this.marca} ${this.modelo} está acelerando!`;
+  },
+};
 
-  console.log("Carro:", meuCarro);
-  console.log(meuCarro.acelerar());
-  
-  console.log("Moto:", minhaMoto);
-  console.log(minhaMoto.acelerar());
+console.log("Carro:", meuCarro);
+console.log(meuCarro.acelerar());
+
+console.log("Moto:", minhaMoto);
+console.log(minhaMoto.acelerar());
+
+const gerenciadorCarros = new GerenciadorVeiculos<Carro>();
+const gerenciadorMotos = new GerenciadorVeiculos<Moto>();
+
+
+gerenciadorCarros.adicionar(meuCarro);
+gerenciadorMotos.adicionar(minhaMoto);
